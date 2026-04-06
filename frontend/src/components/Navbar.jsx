29d0dc831848d946d94d1c8d08b5afd0ce32ccf7
iamanu26 <anuragdubey26202@gmail.com>
2026-04-06 18:19:12 +0530

    fix: correct button text from "Get Start" to "Get Started" in Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -28,7 +28,7 @@ function Navbar() {
           ) : (
             <>
               <Link to="/login" style={{ textDecoration: "none" }}>
-                <button style={styles.primaryBtn}>Get Start</button>
+                <button style={styles.primaryBtn}>Get Started</button>
               </Link>
             </>
           )}
